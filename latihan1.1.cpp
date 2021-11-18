7dfc13a8cbe751a1a0e51bef2daa0d80d6ce1b2c
Indira Rully <navrida.indira@gmail.com>
2021-11-18 10:22:40 +0700

    Menambahkan file

---
+++ latihan1.1.cpp
@@ -0,0 +1,23 @@
+latihan 1.1
+
+#include<iostream.h>
+#include<conio.h>
+
+/**
+*bundet.com
+*Menentukan Nilai terbesar dari 2 input bilangan
+*/
+
+void main() {
+    int a, b;
+    cout << "Menentukan Nilai terbesar 2 buah input \n";
+    cout << "masukkan nilai 1 : "; cin >> a;
+    cout << "masukkan nilai 2 : "; cin >> b;
+
+    if ( a > b )
+        cout << "nilai terbesar  : " << a << endl;
+    else
+        cout << "nilai terbesar  : " << b << endl;
+
+    getch();
+}
